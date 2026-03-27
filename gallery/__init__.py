37fcf255e782c60e29a3fd9133a19bab015d237d
Leon-Rossi <leon.rossi@icloud.com>
2026-03-27 02:47:13 +0100

    fixed requirements.txt, and a randomly added sql function

--- gallery/__init__.py
+++ gallery/__init__.py
@@ -3,27 +3,12 @@ import os
 from flask import Flask, redirect, render_template, url_for
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, static_url_path="/static", instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "gallery.sqlite"),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    os.makedirs(app.instance_path, exist_ok=True)
 
     @app.route("/")
     def home():
-        return os.listdir("./gallery/static/gallery")
         return redirect(url_for("person"))
 
     @app.route("/person")
